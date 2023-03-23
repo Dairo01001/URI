c36db05ba7f021698e6df123f4b8b00552bfde02
DairoGN <dair.garcia@udla.edu.co>
2023-03-22 23:39:36 -0500

    first commit

---
+++ CPP/temp.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// g++ -o main temp.cpp && ./main
+
+int main()
+{
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+
+  return 0;
+}
